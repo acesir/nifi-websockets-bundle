3968dca09291b3738b98393ecfb79ada9c3f5675
acesir <adis.cesir@gmail.com>
2016-04-20 12:36:09 -0500

    initial

---
+++ nifi-websockets-processors/src/test/java/org/apache/nifi/processors/websockets/MyProcessorTest.java
@@ -0,0 +1,57 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.nifi.processors.websockets;
+
+import org.apache.nifi.util.MockFlowFile;
+import org.apache.nifi.util.TestRunner;
+import org.apache.nifi.util.TestRunners;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+
+public class MyProcessorTest {
+
+    private TestRunner testRunner;
+
+    @Before
+    public void init() {
+        testRunner = TestRunners.newTestRunner(MyProcessor.class);
+    }
+
+    @Test
+    public void testProcessor() {
+        final TestRunner runner = TestRunners.newTestRunner(new MyProcessor());
+        //runner.setProperty(ConvertJSONtoCSV.DELIMITER, "|");
+        runner.setProperty(MyProcessor.MY_PROPERTY,"konj");
+        //runner.setProperty(ConvertJSONtoCSV.EMPTY_FIELDS, "NULL");
+        //runner.enqueue(Paths.get("src/test/resources/simple.json"));
+        runner.run();
+        runner.assertAllFlowFilesTransferred(MyProcessor.MY_RELATIONSHIP);
+
+        try {
+            final MockFlowFile out = runner.getFlowFilesForRelationship(MyProcessor.MY_RELATIONSHIP).get(0);
+            out.assertContentEquals(new String(Files.readAllBytes(Paths.get("src/test/resources/simple.csv"))));
+        }
+        catch (Exception ex){
+            System.out.println(ex);
+        }
+    }
+
+}
